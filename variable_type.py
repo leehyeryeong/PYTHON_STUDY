ced918b6660c9c36b66d364ebbd13e42f2949068
leehyeryeong <w2108@e-mirim.hs.kr>
2022-09-22 14:50:39 +0900

    변수 내용만 바꿔서 출력하기

--- variable_type.py
+++ variable_type.py
@@ -1,8 +1,8 @@
 # 애완동물을 소개해 주세요~
-animal = "강아지"
-name = "연탄이"
+animal = "고양이"
+name = "해피"
 age = 4
-hobby = "산책"
+hobby = "낮잠"
 is_adult = age >= 3
 
 print("우리집 " + animal + "의 이름은 " + name + "에요")
