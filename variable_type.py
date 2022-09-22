9cbda22e8e8246eb8f3e0fb2ddbc731e99edc047
leehyeryeong <w2108@e-mirim.hs.kr>
2022-09-22 14:53:27 +0900

    출력할 내용 + 말고 ,로 이어보기

--- variable_type.py
+++ variable_type.py
@@ -6,5 +6,6 @@ hobby = "낮잠"
 is_adult = age >= 3
 
 print("우리집 " + animal + "의 이름은 " + name + "에요")
-print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
-print(name + "는 어른일까요?" + is_adult)
+# print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
+print(name + "는 어른일까요?" + str(is_adult))
